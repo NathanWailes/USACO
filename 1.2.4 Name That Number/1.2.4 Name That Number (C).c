c60ae6c6b7572445ef4b0cfe2a5a5f431611675e
Nathan Wailes <nathan.wailes@gmail.com>
2016-03-20 17:07:34 -0700

    1.2.4 Implemented 'get_the_cows_number_given'

--- 1.2.4 Name That Number/1.2.4 Name That Number (C).c
+++ 1.2.4 Name That Number/1.2.4 Name That Number (C).c
@@ -1,51 +1,30 @@
+/* 2016.03.20 - I finally got the function 'get_the_cows_number_given()' working.
+Next step: implement one of the other functions found in the Python solution (which works). */
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
-const char* get_the_cows_number_given(const char* the_input_filename) {
-    //INPUT FORMAT
-    //A single line with a number from 1 through 12 digits in length.
-    //SAMPLE INPUT (file namenum.in)
-    //4734
+char* get_the_cows_number_given(const char* the_input_filename) {
+    /* 2016.03.20 - This function should be good-to-go. */
     FILE *the_input_file;
     the_input_file = fopen("namenum.in", "r");
-    const char* the_cows_number[13];
+    char* the_cows_number = malloc(13);
     int i = 0;
     char next_character_in_file;
     while ((next_character_in_file = fgetc(the_input_file)) != '\n') {
-        the_cows_number[i] = "f";
-        printf("hi\n");
+        the_cows_number[i] = next_character_in_file;
+        putchar(next_character_in_file);
     }
 
-/*
-    //    the_cows_number
-*/
     fclose(the_input_file);
-    return(the_input_filename);
+    return(the_cows_number);
 }
 
 int main()
 {
     const char* the_input_filename = "namenum.in";
-
     const char* the_cows_number = get_the_cows_number_given(the_input_filename);
 
-    /*
-    const char* the_dictionarys_filename = "dict.txt";
-    get_dictionary_given(the_dictionarys_filename);
-
-*/
     printf(the_cows_number);
     return(0);
 }
-
-
-/*
-
-    int day, year;
-    char weekday[20], month[20], dtm[100];
-
-    strcpy( dtm, "Saturday March 25 1989" );
-    sscanf( dtm, "%s %s %d %d", weekday, month, &day, &year );
-
-    printf("%s %d, %d = %s\n", month, day, year, weekday );
-*/
